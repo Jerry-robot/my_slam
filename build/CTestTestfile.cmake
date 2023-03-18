c87700f10308a2533703b06c35f92a04a91016d5
Jerry <1374450529@qq.com>
2023-03-18 22:11:03 +0800

    readme

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gjw/my_slam/src
+# Build directory: /home/gjw/my_slam/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/gjw/my_slam/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/gjw/carto_ws/cartographer_detailed_comments_ws/install_isolated;/opt/ros/melodic'.split(';') if '/home/gjw/carto_ws/cartographer_detailed_comments_ws/install_isolated;/opt/ros/melodic' != '' else []
